4de3b6744fb7c0a8a68c3990781d9af08588144a
nash <nash34@gmail.com>
2019-10-29 00:33:03 +0500

    init

---
+++ setup.py
@@ -0,0 +1,23 @@
+import os
+from setuptools import setup
+
+with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
+    README = readme.read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='telegram',
+    version='0.1.0',
+    packages=['telegram'],
+    include_package_data=True,
+    license='',  # example license
+    description='',
+    long_description=README,
+    url='',
+    author='Nail Sharipov',
+    author_email='nash34@gmail.com',
+    classifiers=[
+    ],
+)
---
+++ telegram/telegram.py
@@ -0,0 +1,22 @@
+import requests
+
+url = "..."
+
+def get_updates_json(request):
+    response = requests.get(request + 'getUpdates')
+    return response.json()
+
+
+def last_update(data):
+    results = data['result']
+    total_updates = len(results) - 1
+    return results[total_updates]
+
+def get_chat_id(update):
+    chat_id = update['message']['chat']['id']
+    return chat_id
+
+def send_mess(chat, text):
+    params = {'chat_id': chat, 'text': text}
+    response = requests.post(url + 'sendMessage', data=params)
+    return response
